b07c6a2f6518be2e593a41c9e68e8281026bdb11
soikelo <soikkeli.heidi@gmail.com>
2025-03-10 18:42:02 +0200

    Create first version

---
+++ script.js
@@ -0,0 +1,248 @@
+const translations = {
+    fi: {
+
+        headertext: `<h1>LINDA SALO</h1>
+            <p>Vasemmistoliiton kuntavaaliehdokas Tampereella 2025</p>`,
+        introduction: `
+         <h2>Kuka olen?</h2>
+                <p>Moi, olen Linda, 40-vuotias paljasjalkainen tamperelainen. Lapsuuteni vietin Tesomalla, teini-iän
+                    Lielahdessa ja nykyisin asun Amurissa. Sukuni tulee Ruskealasta Karjalasta, ja Saloja vaikuttaa
+                    edelleen myös Nokialla.</p>
+                <p>Tällä hetkellä työskentelen energia-alalla asiakasneuvojana, ja olen opiskellut Tampereen
+                    yliopistossa kansainvälistä politiikkaa ja valtio-oppia. Tätä ennen olin töissä mm. Tampere-talossa,
+                    ja sitä ennen pitkään Stockmannilla.</p>
+                <p>Eniten innostun kulttuurista ja musiikista, viherkasveista, tietovisoista ja uuden oppimisesta.
+                    Luontaista uteliaisuuttani on hyvä toteuttaa yhdessä pienen lapseni kanssa, käydä kirjastossa tai
+                    uimahallissa, kasvattaa omia tomaatteja pihassa, askarrella ja leikkiä legoilla.</p>`,
+        justification:
+            `<h2>Miksi olen ehdolla?</h2>
+                <p>Lähdin kuntavaaleihin ehdolle, sillä haluan turvata oman kotikaupunkini julkiset palvelut kuten
+                    neuvolan, päiväkodin, terveydenhuollon, koulutuksen laadun, ja saavutettavan lähiluonnon.</p>
+                <p>Eniten suututtaa epäoikeudenmukaisuus ja epäreiluus. Minulle on maksettu vuosien ajan liian pientä
+                    palkkaa (sain sen onneksi takautuvasti), olen ollut 0h-sopimuksella töissä useamman vuoden ja
+                    taloudellinen toimeentuloni on ollut hyvin epävarmaa varsinkin opiskeluaikoina. Palkalla tai
+                    eläkkeellä pitäisi voida elää. On kuitenkin elämäntilanteita, joissa meistä jokainen saattaa tarvita
+                    vaikkapa asumis- tai toimeentulotukea.</p>
+                <p>On aivan käsittämätöntä, että yhteisesti maksetuilla veroilla ei muka saa hyvää terveydenhuoltoa ja
+                    hoitoa silloin kun tarvitsee, että lapset joutuvat opiskelemaan homekouluissa aivan liian suurissa
+                    ryhmissä, perheet jonottavat ruokajonoissa ja asunnottomuus vain kasvaa. Näihin kaikkiin on
+                    mahdollista puuttua ja ennaltaehkäistä ongelmia, jos vain on poliittista tahtoa.</p>`,
+        slogan:
+            `ARVONI OVAT TOIVO, SIVISTYS JA ROHKEUS`,
+        values1: `<h2>Toivo</h2>
+
+                <p>Vaikka sota, vallanhimo ja fasismi vyöryvät joka tuutista päälle, minkä seurauksena tekisi mieli
+                    vetää peitto korviin ja piiloutua, niin hämmennyksen ja lamaannuksen sijaan nyt jos koskaan pitää
+                    yrittää toimia.</p>
+                <p>Viisivuotiaani mielestä nukkuminen öisin on turhaa, koska jokainen uusi päivä on niin ihmeellinen ja
+                    innostava. Muistan kaukaisesti tuon tunteen ja toivoisin, että voisimme kaikki ennemmin odottaa
+                    päivän seikkailuja ja mahdollisuuksia avoimin ja turvallisin mielin kuin pelätä, mitä hirveää tänään
+                    tapahtuu.</p>
+                <p>Toivo on voimavara, jonka avulla uskaltaa unelmoida ja kuvitella parempaa tulevaisuutta. Olen ainakin
+                    sen verran velkaa jälkikasvulleni, että yritän esimerkin avulla osoittaa keinoja vaikuttaa,
+                    edustuksellisen demokratian rinnalla toki muillakin tavoin.</p>
+                <p>Haluan, etteivät lapset ja nuoret menetä kipinäänsä vaan kokevat, että heillä on väliä, että heitä
+                    kuunnellaan ja että maailma kuuluu heille. Että osaamme hoitaa yhteiskunnallisia asioita yhdessä.
+                    Että meillä on tulevaisuus, ja että raha ei ole kaiken mittari.</p>`,
+        values2: `<h2>Sivistys</h2>
+
+                <p>Sivistys on enemmän kuin koulutusta. Se on huolenpitoa niistä, jotka eivät välttämättä osaa tai pysty
+                    pitämään huolta itsestään. Lapset, vanhukset, vammaiset, vähemmistöt – sivistys on sitä, että
+                    pidämme heikommassa asemassa olevien puolia ja osoitamme tukea niille, jotka sitä tarvitsevat.</p>
+                <p>Koskaan ei tiedä, jääkö itse työttömäksi, invalidisoituuko tai joutuuko laitokseen, joten empatiakyky
+                    ja solidaarisuus ovat vastavuoroisen tärkeitä. Se, että kannustamme toisiamme ja saamme apua
+                    läheisille.</p>
+                <p>Omia verovarojani pitäisi näin ollen kohdentaa nimenomaan laadukkaaseen varhaiskasvatukseen,
+                    kirjastoihin, kulttuuriin, opetukseen, terveydenhuoltoon ja kaikkiin niihin julkisiin palveluihin,
+                    jotka ovat suomalaisen yhteiskunnan kivijalka.</p>
+                <p>Toivon, että olemme jatkossakin kiinnostava ja laadukasta elämää tarjoava kaupunki ja kunta.
+                    Tampereesta tekee omaleimaisen se, että täällä voi mennä oopperasta punkkikeikalle ja korispelistä
+                    yleiseen saunaan.</p>
+                <p>Haluan, että meillä on jatkossakin turvallinen ja kaikille saavutettava kunta, jossa voi elää hyvää
+                    elämää ilman pelkoa. Näin ollen esimerkiksi turvakotipaikkojen määrää pitää lisätä ja kunnan tulee
+                    tarjota matalan kynnyksen palveluita lähisuhdeväkivaltaa kokeneille, sekä keskittyä
+                    ennaltaehkäisevään työhön väkivaltaan puuttumiseksi.</p>`,
+        values3: `<h2>Rohkeus</h2>
+                <p>Tämän hetken ilmapiiri tuntuu olevan epävarma ja jopa taantumuksellinen. Rohkeudella ja
+                    päättäväisyydellä tehdään meitä kaikkia kuntalaisia koskettavia elämänlaatuun vaikuttavia hyviä
+                    päätöksiä. Näitä ovat esimerkiksi esteettömyyden ja ympäristön aiheuttaman aistiärsykekuormituksen
+                    huomioiminen kaupunkisuunnittelussa, feministinen auraus talvikunnossapidossa (kevyt liikenne ja
+                    pyörätiet) sekä gynekologiset palvelut terveysasemille, ja maksuton ehkäisy alle 25-vuotiaille.</p>
+                <p>Kaipaan uskaltamista ja raikkaita uusia ideoita kaupunkikuvaan, kuten vaikkapa kokonaan uutta
+                    siirtolapuutarha-aluetta tai viljelypalstoja halukkaille, japanilaista puutarhaa, talvipuutarhaa tai
+                    kirsikkapuistoa. Lasten leikkipuistoissa tulisi olla kaikissa varjoa tuovia puita, ja huoltaminen
+                    ajan tasalla. Onkiniemeen mahtuisi aivan varmasti uusi kansankypylä, ja lajikirjosta voidaan
+                    huolehtia mm. bussipysäkkien turvekatoilla ja hulevesistä kosteikoilla.</p>
+                <p>Haluan, että kulttuurintekijöillä ja taiteilijoilla on tilaa olla luovia, että lapset ja nuoret
+                    saavat harrastaa perheen tulotasosta riippumatta ja että asunnottomuutta ja ruokajonojen pituutta
+                    yritetään hillitä. Ja aika on jo omalle muistomerkille, kadulle tai aukiolle edesmenneelle
+                    pioneeriartistille Kikalle! </p>`,
+        contact: `<h2>Ota yhteyttä</h2>
+                <p>Haluatko vaikuttaa jollakin tavalla konkreettisesti?</p>
+                <p>Nyt on tilaisuutesi auttaa ja lahjoittaa kuntavaalikampanjaani, ihan pienkin summa riittää. Rahat
+                    menevät painokuluihin ja markkinointiin.</p>
+                <p>Vaalitili: FI62 5730 0821 5505 28</p>
+                <p>Mobilepay lipas: 4182SN</p>
+                <p>Mikäli haluat kysyä jotain tai tukea kampanjaani muulla tavoin, voit ottaa yhteyttä sähköpostitse: linda.m.salo(at)gmail.com`,
+
+    },
+    en: {
+        headertext: `<h1>LINDA SALO</h1>
+            <p>Left Alliance candidate for municipal elections in Tampere 2025</p>`,
+        introduction: `<h2>Who am I?</h2>
+        <p>I'm Linda, 40 years old and born and raised in Tampere. I spent my
+        childhood with my single mother in Tesoma and my teenage years
+        in Lielahti, and nowadays I live in Amuri. My roots are in Ruskeala,
+        Karjala, and there are still members of the Salo family living in
+        Nokia.</p>
+        <p>I currently work as a customer adviser in the energy sector and I
+        have studied International Relations and Political Science at the
+        University of Tampere. Previously, I have worked at Tampere Hall
+        and Stockmann, among others, for long periods of time.</p>
+        <p>Among the things that excite me the most are culture and music,
+        houseplants, quizzes and learning. I satisfy my natural curiosity with
+        my young child: we go to the library and the local swimming pool,
+        do arts and crafts and play with Legos.</p>`,
+        justification: `<h2>Why am I standing for election?</h2>
+        <p>I am standing for election in the municipal elections because I
+        primarily want to secure the public services in my home town such
+        as maternity clinics, daycare, healthcare, the quality of education
+        and accessible local nature.</p>
+        <p>It is utterly incomprehensible that jointly paid taxes can't guarantee
+        us good healthcare and treatment when we need it, that children
+        have to study in moldy school buildings in way too large groups,
+        that families have to stand in line to be provided food aid and that
+        the number of homeless people is yet increasing. It is possible to
+        tackle and prevent all said problems if there is political will.</p>
+        <p>What aggravates me the most is injustice and unfairness, especially
+        in working life. I was paid too little for years (luckily, it was
+        compensated retroactively), I have worked on a zero-hour
+        employment contract for several years, and my income has been
+        very uncertain especially during my years of studying.</p>
+        <p>Everyone should be able to live off their salary or pension in
+        Finland. There are, however, situations in life where anyone of us could need housing allowance or social assistance, for instance. It
+        is totally wrong that people are being punished for being
+        unemployed and that the people who have the least to start with are
+        the ones whose benefits are being cut.</p>
+        <p>I'm an active and productive person by nature, but I may be a little
+        snippy at times due to my frankness. I like solving problems and
+        performing the best I can at work. I am an open person and I
+        appreciate honesty and stand-up responsibility.</p>`,
+        slogan: `MY VALUES ARE HOPE, CIVILIZA&shy;TION AND COURAGE`,
+        values1: `<h2>Hope</h2>
+        <p>According to my 5-year-old, it's useless to sleep at night because
+        every new day is so amazing and inspiring. I, too, remember the
+        feeling and wish we all could look forward to each day's adventures
+        with an open and comfortable mind rather than be wary of the
+        possible horrors to come.</p>
+        <p>In the news, we learn how war, greed for power and fascism are
+        taking the world by storm, which makes me want to crawl under a
+        blanket and hide away. However, hope is the one asset that gives
+        me courage to dream of and imagine a better future. I owe it to my
+        posterity and future generations to do what I can in order to make a
+        difference.</p>
+        <p>I don't want children and the young to lose their spark. I want them
+        to feel like they matter and are heard and that the world belongs to
+        them. That we are capable of taking care of societal matters
+        together. That we all have a future where everything isn't weighed
+        in money. Dreams and idealistic ideas are not just vagaries but an
+        option if we work towards achieving our goals in a realistic manner.</p>
+        <p>The time to take action and not paralyze is now or never, and that is
+        why I'm standing for election.</p>`,
+        values2: `<h2>Civilization</h2>
+        <p>Civilization is more than education. It is caring for those who may
+        not be able to take care of themselves. Children, senior citizens,
+        people with disabilities, minorities – civilization means that we stand
+        up for those who are vulnerable and show our support for those
+        who are in need of it.</p>
+        <p>Taxpayers' money should be targeted to quality early childhood
+        education and care, libraries, culture, education, healthcare and all
+        the public services that are the foundation of the Finnish society.
+        Anyone can lose their job, face health issues, become disabled or
+        be placed in institutional care, so empathy and solidarity are
+        mutually important.</p>
+        <p>In the future, as we have so far, I want all of us to be able to lead a
+        good, fearless life in a city that is safe and accessible for everyone.
+        Ergo, shelter home capacity needs to be increased and victims of
+        domestic and immediate partner violence should be offered low-
+        threshold services. In addition, we need to focus on preventive
+        measures to stop the violence.</p>
+        <p>Despair can push people into taking desperate actions, and in order
+        to maintain social harmony, we need to tackle poverty in families
+        with children and mental health issues in young people, fund
+        preventive substance abuse work and enable adequate livelihood
+        for all.</p>`,
+        values3: `<h2>Courage</h2>
+        <p>Courage and determination enable making good decisions that
+        affect all residents' quality of life, such as paying attention to
+        accessibility and overstimulation from the environment in urban
+        planning, winter maintenance on bike lanes and walkways,
+        gynaecological services at health clinics and free contraception for
+        everyone under the age of 25.</p>
+        <p>I long for courage and fresh new ideas in the cityscape, such as a
+        whole new community garden with moderate prices, accessible
+        allotments or a new public bath. There should be trees for shade at
+        all playgrounds. Biodiversity can be maintained by bus stops with
+        turf roofs and bee meadows on wastelands, and flood water can be
+        restrained with appropriately built storm water wetlands.</p>
+        <p>I want artists and people in the cultural line of business to have
+        inexpensive workspaces to be creative in and children and the
+        young to have access to hobbies regardless of the income of their
+        families. It's about time we arrange a contest to design a monument
+        for the late pioneer artist Kikka in the city of Manserock!</p>
+        <p>Tampere has developed immensely since I was a child and was
+        waiting for a bus that ran twice an hour to take me from Tesoma to
+        Pispala where I studied in a music-oriented class. Now we have a constantly expanding tram network and a commuter train. Public
+        transport is not only ecological and frees up city space for other
+        usage, but also reduces particulate pollution and, consequently,
+        improves air quality.</p>
+        <p>I want Tampere to be an attractive city that provides a life of quality
+        for everyone and not just for the wealthy part of the population.
+        What makes Tampere so original is that you can go from the opera
+        to a punk rock gig or from a basketball game to pick blueberries in
+        the forest. Each one of us should have the chance to lead a life to
+        our liking and have a carefree everyday life. This will be possible if
+        we stand up for each other and reach for the future with an
+        intergenerational take.</p>`,
+        contact: `<h2>Contact</h2>
+        <p>Do you want to have a concrete impact?</p>
+        <p>Now is your chance to help and donate to my municipal elections campaign, even the smallest amounts helps! The donations are used for printing and marketing costs.</p>
+        <p>Bank account: FI62 5730 0821 5505 28</p>
+        <p>Mobilepay box: 4182SN</p>
+        <p>If you want to ask something or support my campaign in other ways, you can contact me via email: linda.m.salo(at)gmail.com</p>`,
+    }
+}
+
+const languageNames = {
+    en: "English",
+    fi: "Suomi",
+}
+
+function updateContent(langData, lang) {
+    document.querySelectorAll("[data-i18n]").forEach((element) => {
+        const key = element.getAttribute("data-i18n");
+        element.innerHTML = langData[key];
+    });
+    const currentLangLink = document.querySelector(`#${lang}`);
+    currentLangLink.innerHTML = languageNames[lang];
+    document.documentElement.lang = lang
+}
+
+function setLanguagePreference(lang) {
+    localStorage.setItem("language", lang);
+    location.reload();
+}
+
+async function changeLanguage(lang) {
+    setLanguagePreference(lang);
+
+    const langData = translations[lang];
+    updateContent(langData, lang);
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    const userPreferredLanguage = localStorage.getItem("language") || "fi";
+    const langData = translations[userPreferredLanguage];
+    updateContent(langData, userPreferredLanguage);
+});
+
